af0660ea22da15309b3c2312950ddeafc3b6aa0a
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-12 17:23:08 -0400

    Assignment 2 initial push to GitHub

--- src/main/java/oop/assignment2/ex29/solution29.java
+++ src/main/java/oop/assignment2/ex29/solution29.java
@@ -1,4 +1,62 @@
 package oop.assignment2.ex29;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
+import java.util.Scanner;
+
 public class solution29 {
+    private static final Scanner in = new Scanner(System.in);
+
+    private static String userInput;
+
+    public static void main(String[] args) {
+
+       String userReturnRate = userInputValidation();
+       int returnRate = Integer.parseInt(userReturnRate);
+
+        outputStatement(returnRate);
+
+    }
+
+    private static String userInputValidation() {
+
+        int count = 1;
+        String userInput;
+
+        do {
+            System.out.print("What is the rate of return? ");
+            userInput = in.nextLine();
+
+            if( userInput.equals("0") ){
+                System.out.println("Sorry, that's not a valid input.");
+            }
+            else if( userInput.matches("[a-zA-Z_]+")){
+                System.out.println("Sorry, that's not a valid input.");
+            }
+            else{
+                break;
+            }
+        } while( count != 0);
+
+        return userInput;
+
+    }
+
+    private static void outputStatement( int returnRate ){
+
+        int totalYears = 72 / returnRate;
+        System.out.print("It will take "+totalYears+" years to double your initial investment.");
+
+    }
 }
+
+
+
+
+
+
+
+
---
+++ src/main/java/oop/assignment2/ex31/solution31.java
@@ -0,0 +1,66 @@
+package oop.assignment2.ex31;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
+import java.util.Scanner;
+
+public class solution31 {
+    private static final Scanner in = new Scanner(System.in);
+
+    private static int userAge;
+    private static int userRestingRate;
+    private static int targetHeartRate;
+
+    public static void main(String[] args){
+
+        System.out.print("What is your age? ");
+        userAge = userNumericalInput();
+        System.out.print("What is your resting heart rate? ");
+        userRestingRate = userNumericalInput();
+
+        targetHeartRateOutput();
+
+    }
+
+    private static int userNumericalInput(){
+
+        int count = 1;
+        String userInput;
+
+        do {
+            userInput = in.nextLine();
+
+            if( userInput.equals("0") ){
+                System.out.println("Sorry, that's not a valid input.");
+            }
+            else if( userInput.matches("[a-zA-Z_]+")){
+                System.out.println("Sorry, that's not a valid input.");
+            }
+            else{
+                break;
+            }
+        } while( count != 0 );
+
+        int numericalOutput = Integer.parseInt(userInput);
+        return numericalOutput;
+    }
+
+    private static void targetHeartRateOutput(){
+
+        System.out.println("Resting Pulse: "+userRestingRate+"\t\t\t\tAge: "+userAge+"");
+
+        System.out.println("Intensity \t\t| Rate");
+        System.out.println("________________|__________________");
+        for( int i = 55; i < 100; i++ ) {
+
+            targetHeartRate = (int) ((((220 - userAge) - userRestingRate) * ( i / 100.0 ) ) + userRestingRate);
+            System.out.println(+i+"%\t\t\t\t| "+targetHeartRate+" bpm");
+            i = i + 4;
+
+        }
+    }
+
+}
