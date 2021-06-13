6c19b6745871fe94603a1d7a5a943c579ddce09c
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-12 21:46:08 -0400

    Assignment 2 initial push to GitHub

--- src/main/java/oop/assignment2/ex38/solution38.java
+++ src/main/java/oop/assignment2/ex38/solution38.java
@@ -5,8 +5,53 @@ package oop.assignment2.ex38;
  *  Copyright 2021 Ivedent Lloyd III
  */
 
+import java.util.Scanner;
+
 public class solution38 {
 
+    private static final Scanner in = new Scanner(System.in);
 
+    public static void main(String[] args){
+
+       String userInput =  arrayInput();
+       char[] inputArray = inputArray(userInput);
+
+       outputStatement(inputArray, userInput);
+    }
+
+
+    private static String arrayInput(){
+
+        System.out.print("Enter a list of numbers separated by spaces: ");
+        String userInput = in.nextLine();
+
+        return userInput;
+
+    }
+
+    private static char[] inputArray(String userInput){
+
+        char[] inputArray = new char[userInput.length()];
+        for( int i = 0; i < userInput.length(); i++ ){
+            inputArray[i] = userInput.charAt(i);
+        }
+        return inputArray;
+    }
+
+    private static void outputStatement(char[] inputArray, String userInput){
+
+        System.out.print("The even numbers are");
+
+        for( int i = 0; i < userInput.length(); i++ ){
+
+            if ( inputArray[i] % 2 == 0 ){
+
+                System.out.print(" "+inputArray[i]);
+
+            }
+
+        }
+
+    }
 
 }
