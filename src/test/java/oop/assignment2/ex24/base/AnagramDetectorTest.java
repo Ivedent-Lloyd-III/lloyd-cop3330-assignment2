ce47d89f00a9a10abc28e71d756d3ed484cd2c69
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-13 13:21:04 -0400

    Assignment 2 initial push to GitHub

---
+++ src/test/java/oop/assignment2/ex24/base/AnagramDetectorTest.java
@@ -0,0 +1,27 @@
+package oop.assignment2.ex24.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnagramDetectorTest {
+
+    @Test
+    void isAnagram_returns_true_for_other_anagrams() {
+        AnagramDetector detector = new AnagramDetector();
+
+        boolean actual = detector.isAnagram("note", "tone");
+
+        assertTrue(actual);
+    }
+
+    @Test
+    void isAnagram_returns_false_for_non_anagrams(){
+        AnagramDetector detector = new AnagramDetector();
+
+        boolean actual = detector.isAnagram("difo", "god");
+
+        assertFalse(actual);
+    }
+
+}
