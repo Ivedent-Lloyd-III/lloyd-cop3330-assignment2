4a2ff224bf8c016d5f16d290e30e84455ada3235
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-12 22:54:29 -0400

    Assignment 2 initial push to GitHub

---
+++ src/main/java/oop/assignment2/ex39/solution39.java
@@ -0,0 +1,20 @@
+package oop.assignment2.ex39;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
+
+import java.util.Scanner;
+
+public class solution39 {
+
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+
+
+    }
+}
